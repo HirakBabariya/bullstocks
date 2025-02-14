6688e2f3b2c2bc05df52804adbae173bba78d72a
Hirak Babariya <82439424+HirakBabariya@users.noreply.github.com>
2025-02-14 16:32:10 -0500

    Update app.ts

--- server/app.ts
+++ server/app.ts
@@ -103,6 +103,7 @@ const init = async () => {
   // Set up WebSocket server for real-time communication
   const io = new Server(httpServer, {
     path: "/socket.io",
+    cors: { origin: "https://bullstocks.vercel.app"}
   });
 
   io.on("connection", (socket) => {
